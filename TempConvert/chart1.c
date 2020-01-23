2f173b832a2a2daa48873aaf4006819854a5ba2c
Gideon Wikina <glw3325@nitron.se.rit.edu>
2020-01-23 10:49:45 -0500

    Preliminary committ TempConvert

---
+++ TempConvert/chart1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int fahrenheit;
+	int celsius;
+	printf("Fahrenheit - Celsius\n");
+	for (int i = 0; i <= 300; i += 20)
+	{
+	    //Statements to be executed repeatedly
+		fahrenheit = i;
+		celsius = (5* (fahrenheit-32))/9;
+		printf("  %d            ", fahrenheit);
+		printf("%d\n", celsius);
+	}
+
+	return celsius;
+}
+
+
