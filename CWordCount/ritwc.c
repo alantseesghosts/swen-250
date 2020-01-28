0ffe409854b2bd320c81e62300b0f14670f02758
Gideon Wikina <glw3325@nitron.se.rit.edu>
2020-01-28 10:35:02 -0500

    My code and Dark and Stormy Night File

---
+++ CWordCount/ritwc.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#define FALSE (0)
+#define TRUE  (1)
+
+int main() {
+	int tot_chars = 0 ;	/* total characters */
+	int tot_lines = 0 ;	/* total lines */
+	int tot_words = 0 ;	/* total words */
+
+	/* REPLACE WITH ADDITIONAL VARIABLE DECLARATIONS YOU NEED */
+
+	/* REPLACE WITH THE CODE FOR WORD COUNT */
+
+	return 0 ;
+}
