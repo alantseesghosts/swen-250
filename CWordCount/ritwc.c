8d06f70a33ce2d1d7591fbad37e36cc25518f050
Gideon Wikina <glw3325@nitron.se.rit.edu>
2020-01-30 09:52:42 -0500

    CwordCount was commited

--- CWordCount/ritwc.c
+++ CWordCount/ritwc.c
@@ -10,9 +10,29 @@ int main() {
 	int tot_lines = 0 ;	/* total lines */
 	int tot_words = 0 ;	/* total words */
 
-	/* REPLACE WITH ADDITIONAL VARIABLE DECLARATIONS YOU NEED */
+	int in_space = 1;
+    	int c, last = '\n';
 
-	/* REPLACE WITH THE CODE FOR WORD COUNT */
+	while ((c = getchar()) != EOF) {
+        last = c;
+        tot_chars++;
+        if (isspace(c)) {
+            in_space = 1;
+            if (c == '\n') {
+                tot_lines++;
+            }
+        } 
+	else {
+            tot_words += in_space;
+            in_space = 0;
+        }
+    }
+    if (last != '\n') {
+        /* count last line if not linefeed terminated */
+        tot_lines++;
+    }
 
-	return 0 ;
+    printf("Lines, Words, Characters\n");
+    printf("%3d     %3d       %3d\n", tot_lines, tot_words, tot_chars);
+    return 0 ;
 }
