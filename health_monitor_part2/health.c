1b5ca1d1738917b8bf702c75ed5a4cc34641a852
Gideon Wikina <glw3325@nitron.se.rit.edu>
2020-04-26 23:09:45 -0400

    Updated Helpers & Updated main (removed case is & replaced with if statements)

--- health_monitor_part2/health.c
+++ health_monitor_part2/health.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include "health.h"
 
+#define MAXLINE 20 //most possible characters in a line
+
 /*
 * Main function for the Health Monitoring System. Primarily responsible for
 * processing input of csv lines and printing as required. Data structures are
@@ -16,7 +18,7 @@ Chartptr patientList = NULL;    /* Define global variable patientList (declared 
 
 void main(){
 
-printf("Welcome to the Health Monitoring System\n\n");
+	printf("Welcome to the Health Monitoring System\n\n");
 
 /*
 *  YOUR CODE GOES HERE:
@@ -28,108 +30,55 @@ printf("Welcome to the Health Monitoring System\n\n");
 *        - Store health data in patient record or print if requested
 *  (4) Continue (1)-(3) until EOF
 */
+	char line[MAXLINE+1];
+	scanf("%[^\n]", line);
+	char splitby[] = ", ";
+	while (!feof(stdin)) {
+		char *token = strtok(line, splitby);
+		int pID = strtol(token, NULL, 10);
+		
+		token = strtok(NULL, splitby);
+		char timeStamp[MAXTIME + 1];
+		strcpy(timeStamp, token);
 
-	int id; //variable to keep track of scanf's id imput
-	char time[MAXTIME + 1]; //variable to keep track of scanf's time input
-	int type; //variable to keep track of the type
-	int value; // variable to keep track of the value
-	int buffIndex; //variable for print statement to keep track of number of buffers
-	int readIndex;//variable for print case to keep track of the readings in the buffer
-	CBuffptr patientHealth; //a buffer pointer to get the health records to add in a new health reading
-	//a character array to keep track of all of the types of readings
-	char* types[] = {"Temperature:", "Heart Rate:", "Systolic Pressure:", "Diastolic Pressure:", "Respiration Rate"};
-	Chartptr chart;//Pointer to make sure that the chart of the patient doesn't equal NULL
-	CBuffptr patientPrint; //A bufferpointer used during the print case
+		token = strtok(NULL, splitby);
+		int typeID = strtol(token, NULL, 10);
 
-//A while loop that will keep scanning until the input is "End of File"
-while(scanf("%i, %8[^,], %i, %i", &id, &time[MAXTIME + 1], &type, &value)!= EOF){
-//switches the case based on the type
-switch(type){
-	//type to reset the patient values
-	case 0:
-		printf("--------------------------------------------------\n");
-		printf("%s: Patient ID = %i reset data\n", &time[MAXTIME + 1], id);
-		printf("--------------------------------------------------\n");
-		//calls a function that just removes the health values but keeps the chart intact
-		removeHealthBuff(id);
-		break;
-	//To avoid  redundancy and set all the cases will to drop until case 5. Here the action will take place.
-	case 1:
-	case 2:
-	case 3:
-	case 4:
-	case 5:
-				chart = getChart(id);
-				//Checks to see if the chart is NULL
-				if(chart == NULL){
-					break;
-				}
-				//if not NULL, it  will get the health type buffer
-				patientHealth = getHealthType(id, type);
-				//if it isn't NULL, then it will add the health reading
-				if(patientHealth != NULL){
-					addHealthReading(patientHealth, &time[MAXTIME + 1], value);
-				}
-				break;		
-	case 6:
-				//checks to see if the chart is NULL
-				chart = getChart(id);
-				if(chart == NULL){
-					break;
-				}
-				//goes through each data type in the types array and print out each of their readings
-				buffIndex = 0;
-				printf("--------------------------------------------------\n");
-				printf("Readings for Patient ID = %i are:\n", id);
-				for(buffIndex; buffIndex < 5; buffIndex++){
-					printf("%s\n", types[buffIndex]);
-					patientPrint = getHealthType(id, buffIndex+1);
-					if(patientPrint == NULL){
-						printf("<none>\n");
-					}
-					else{
-						readIndex = 0;
-						if(patientPrint->end < MAXREADINGS){
-							for(readIndex; readIndex < patientPrint->end; readIndex++){
-								if(buffIndex == 0){
-									printf("%s: %.1f\n", patientPrint->reading[readIndex].timestamp, patientPrint->reading[readIndex].value / 10.0);
-								}
-								else{
-									printf("%s: %i\n", patientPrint->reading[readIndex].timestamp, patientPrint->reading[readIndex].value);
-								}
-							}
-						}
-						else{
-							for(readIndex; readIndex < MAXREADINGS; readIndex++){
-                                                                if(buffIndex == 0){
-                                                                        printf("%s: %.1f\n", patientPrint->reading[readIndex].timestamp, patientPrint->reading[readIndex].value / 10.0);
-                                                                }
-                                                                else{
-                                                                        printf("%s: %i\n", patientPrint->reading[readIndex].timestamp, patientPrint->reading[readIndex].value);
-                                                                }
-                                                        }
-						}
-					}
-				}
-				printf("--------------------------------------------------\n");
-				break;
-	case 7:
-				//Checks in a patient
-				printf("--------------------------------------------------\n");
-				addPatient(id);
-				printf("%s: PatientID = %i checking in\n", &time[MAXTIME + 1], id);
-				printf("--------------------------------------------------\n");
-				break;
-	case 8:
-				//checks out a patient 
-				printf("--------------------------------------------------\n");
-				printf("%s: Patient ID = %i  checking out\n", &time[MAXTIME + 1], id);
-				printf("--------------------------------------------------\n");
-				removePatient(id);
-				break;			
+		token = strtok(NULL, splitby);
+		int val = strtol(token, NULL, 10);
 
-}
-}
-printf("\nEnd of Input\n");
+		if(typeID == 7){
+			addPatient(pID);
+			puts("--------------------------------------------------");
+			printf("%s: Patient ID = %d checking in\n", timeStamp, pID);
+			puts("--------------------------------------------------");
+
+		} else if (typeID == 9){
+			addHealthType(pID, val);	
+		} else if (typeID >= 1 && typeID <= 5) {
+			CBuffptr buffer = getHealthType(pID, typeID);
+			addHealthReading(buffer, timeStamp, val);
+		} else if (typeID == 6) {
+			puts("\n--------------------------------------------------");	
+			printPatientData(pID);
+			puts("--------------------------------------------------");
+		} else if (typeID == 8) {
+			checkout(pID);
+			puts("--------------------------------------------------");
+			printf("%s: Patient ID = %d checking out\n", timeStamp, pID);
+			puts("--------------------------------------------------");
+
+		} else if (typeID == 0) {
+			removePatient(pID);
+			puts("--------------------------------------------------");
+			printf("%s: Patient ID = %d reset data\n", timeStamp, pID);
+			puts("--------------------------------------------------");
+		}
+
+
+		scanf(" %[^\n]", line);
+	}
+
+	printf("\nEnd of Input\n");
 
 }
--- health_monitor_part2/health_util.c
+++ health_monitor_part2/health_util.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "health.h"
 
 /*
@@ -11,6 +12,8 @@
 * static void myOptionalFunc();   // EXAMPLE 
 *
 */
+void printPatientData(int ID);
+void checkout(int patientID);
 
 /*
 * addPatient: check-in a new patient
@@ -22,17 +25,20 @@
 */
 void addPatient( int patientID ){
   
-  /* YOUR CODE HERE */
-	Chartptr patientChart = getChart(patientID);
-	if(patientChart == NULL){
-		Chartptr newPatient = malloc(sizeof(Chart));
-		newPatient->id = patientID;
-		newPatient->buffer = NULL;
-		newPatient->next = patientList;
-		patientList = newPatient;
+	Chartptr newChart = malloc(sizeof(Chart));
+	newChart -> id = patientID;
+	newChart -> buffer = NULL;
+	newChart -> next = NULL;
+	
+	if(patientList == NULL){
+		patientList = newChart;
+		newChart -> next = patientList;
+	} else {
+		Chartptr temp = patientList -> next;
+		patientList -> next = newChart;
+		newChart -> next = temp;
 	}
-
-  }
+}
 
 /*
 * addHealthType: add a new health type buffer for a given patient
@@ -42,39 +48,43 @@ void addPatient( int patientID ){
 */
 void addHealthType( int patientID, int newType ){
   
-  /* YOUR CODE HERE */
-	CBuffptr healthType = getHealthType(patientID, newType);
+	CBuffptr newBuff = malloc(sizeof(CircularBuffer));
+	newBuff -> type = newType;
+	newBuff -> start = 0;
+	newBuff -> end = 0;
+	
 	Chartptr patient = getChart(patientID);
-	if(healthType == NULL && patient != NULL){
-		CBuffptr newHealthType = malloc(sizeof(CircularBuffer));
-		newHealthType->type = newType;
-		newHealthType->start = 0;
-		newHealthType->end = 0;
-		newHealthType->next = patient->buffer;
-		patient->buffer = newHealthType;
+	CBuffptr curs = patient -> buffer;
+	
+	if (curs == NULL) {	
+		patient -> buffer = newBuff;
+		newBuff -> next = newBuff; 
+	} else {
+		while(curs -> next != NULL && curs -> next != patient -> buffer) {
+			curs = curs -> next;
+		}
+		curs -> next = newBuff;
+		newBuff -> next = patient -> buffer;
 	}
-  }
+}
   
 /*
 *  getChart: given a patientID, return a pointer to their Chart
 */
 Chartptr getChart( int patientID ){
-  	Chartptr foundChart = NULL;
-	Chartptr iterator = patientList;
-	while(iterator != NULL){
-		if(iterator->id == patientID){
-			foundChart = iterator;
-			return foundChart;
+	Chartptr foundChart = patientList;
+
+	while(foundChart != NULL){
+		if(foundChart -> id == patientID){ 
+			break;
+		} else if ( foundChart -> next == patientList) {
+			return NULL;
 		}
-		else{
-			iterator = iterator->next;
-		}
+		foundChart = foundChart -> next;
 	}
   
-  /* YOUR CODE HERE */
-  
-  return foundChart;
-  }
+	return foundChart;
+}
 
 /* 
 *  getHealthType: given a patientID & healthType, return a pointer 
@@ -82,33 +92,32 @@ Chartptr getChart( int patientID ){
 *  for that patient, return NULL
 */
 CBuffptr getHealthType( int patientID, int healthType ){
- 	CBuffptr foundType = NULL;
-  	Chartptr patientChart = getChart(patientID);
-	CBuffptr buff = patientChart -> buffer;
-	while( buff != NULL){
-		if( buff->type == healthType){
-			foundType = buff;
-			return foundType;
-		}
-		else{
-			buff = buff->next;
-		}
+	CBuffptr foundType = NULL;
+	Chartptr patient = getChart(patientID);
+
+	if (patient -> buffer != NULL){
+		CBuffptr curs = patient -> buffer;   
+		do {
+			if(curs -> type == healthType){
+				foundType = curs; 
+				break;
+			}
+			curs = curs -> next;
+		}while(curs != patient -> buffer);
 	}
-  
-  /* YOUR CODE HERE */
-  
-  return foundType;
-  }
+	return foundType;
+}
  
 /*
 *  addHealthReading: given a pointer to CircularBuffer, add the passed
 *  timestamp and health data type reading to the buffer
 */
 void addHealthReading( CBuffptr buffer, char* timestamp, int reading ){
-  	/* YOUR CODE HERE */
-	buffer->reading[(buffer->end)%10].value = reading;
-	strcpy(buffer->reading[(buffer->end)%10].timestamp, timestamp);
-	buffer->end += 1;
+	
+ 	Element *entry = &(buffer -> reading[buffer -> end]);
+	strcpy(entry -> timestamp, timestamp);
+	entry -> value = reading; 
+	buffer -> end++;
 
 }
   
@@ -116,42 +125,25 @@ void addHealthReading( CBuffptr buffer, char* timestamp, int reading ){
 *  removePatient: check-out an existing patient
 *	  (1) delete the patient's Chart & accompanying 
 *         health data readings.
-*     (2) update the list of current patients
+*     	  (2) update the list of current patients
 */
-void removePatient( int patientID ){
-
-  	/* YOUR CODE HERE */
+void removePatient(int patientID){
 	Chartptr patient = getChart(patientID);
-	Chartptr update = patientList;
-	CBuffptr curr = patient->buffer;
-	Chartptr test;
-	CBuffptr currNext;
-	while(curr != NULL){
-		currNext = curr->next;
-		free(curr);
-		curr = currNext;
+ 
+	if(patient != NULL && patient -> buffer != NULL) {
+		CBuffptr curs = patient -> buffer -> next; 
+
+		while(curs != patient -> buffer){
+
+			CBuffptr temp = curs -> next;
+			free(curs);
+			curs = temp;
+		} 
+		free(patient -> buffer);
+		patient -> buffer = NULL;
 	}
-	patient->buffer = NULL;
-	if(update->id == patientID){
-		patientList = patientList->next;
-		update->next =  NULL;
-	        patient->next = NULL;
-        	free(patient);
-	}
-	else{
-		while(update->next->id != patientID){
-                update = update->next;
-	        }
-        	update->next = update->next->next;
-	        patient->next = NULL;
-        	free(patient);
-	}
-	test = patientList;
-	while(test != NULL){
-		printf("UPDATED LIST: %i", test->id);
-		test = test->next;
-	}
-  }
+}
+
  
 /*
 * Optional helper functions defined starting here:
@@ -159,14 +151,50 @@ void removePatient( int patientID ){
 * static void myOptionalFunc(){ }  // EXAMPLE
 *  
 */
-void removeHealthBuff( int patientID ){
-	Chartptr patient = getChart( patientID );
-	CBuffptr curr = patient->buffer;
-	CBuffptr currNext;
-	while( curr != NULL){
-		currNext = curr->next;
-		free(curr);
-		curr = currNext;
+
+void checkout( int patientID ){
+	
+	removePatient(patientID);
+
+	Chartptr patient = getChart(patientID);
+
+	if (patient != NULL){
+		if ( patient -> next -> id == patient -> id) { 
+			patientList = NULL;
+			free(patient);
+		} else {
+			while (patientList -> next != patient){ 
+				patientList = patientList -> next;
+			} 
+			patientList -> next = patient -> next;
+			free(patient);
+		}
 	}
-	patient->buffer = NULL;
+}
+
+void printPatientData(int ID){
+	printf("Readings for Patient ID = %d are:\n", ID);
+	CBuffptr type;
+	int i, k;
+	char *titles[] = {"Temperature", "Heart Rate", "Systolic Pressure", "Diastolic Pressure", "Respiration Rate"};
+	
+	for (k = 1; k <= 5; k++){
+		printf("%s:\n", titles[k - 1]);
+		type = getHealthType(ID, k);
+		if (type == NULL || type -> start == type -> end){
+			puts("<none>");	
+		} else {
+			if ( k == 1 ){ // Temperature requires a double to be printed
+				for (i = type -> start; i < type -> end; i++){
+					printf("%s: %.1f\n", type -> reading[i].timestamp, type -> reading[i].value / 10.0);
+				}	
+			} else {
+				for (i = type -> start; i < type -> end; i++){
+					printf("%s: %d\n", type -> reading[i].timestamp, type -> reading[i].value);
+				}
+
+			}
+		}
+
+	}
 }
